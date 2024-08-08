b5830e85381a04a6e9ca5bf4fe8d155b975ada27
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-08-08 17:00:26 +0200

    add proto-slider and skeleton

--- app/ui/minimap.tsx
+++ app/ui/minimap.tsx
@@ -5,13 +5,13 @@
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-interface MapProps {
+interface MiniMapProps {
     url: string
 }
 
-export default function Map({ url }: MapProps) {
+export default function MiniMap({ url }: MiniMapProps) {
     return (
-        <MapContainer center={[48.8566, 2.3522]} zoom={5} className="w-2/3 h-[500px] mb-10">
+        <MapContainer center={[48.8566, 2.3522]} zoom={5} className="w-[400px] h-[200px] mb-10 flex-grow-0 flex-shrink-0 basis-1/5">
             <TileLayer
                 url={url}
             // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
---
+++ app/ui/slider.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import { maps } from '@/app/lib/data'
+
+const MiniMap = dynamic(() => import('@/app/ui/minimap'), { ssr: false });
+
+export default function Slider() {
+
+    return (
+        <>
+            <div className="w-full h-[200px] overflow-hidden mb-10">
+                <div className="flex">
+                    {maps.map((map, index) => (
+                        <MiniMap key={index} url={map.url} />
+                    ))}
+                </div>
+            </div>
+
+        </>
+    )
+
+}
