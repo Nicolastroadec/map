cd581d0cb484d89369f6566a5c51165ddf5ac77d
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-08-08 17:07:08 +0200

    update skeleton homemade

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ import { maps } from '@/app/lib/data';
 
 import Slider from '@/app/ui/slider';
 
-import { Skeleton } from "@nextui-org/skeleton";
 import { Suspense } from 'react';
+
+import { CardsSkeleton } from '@/app/ui/skeletons';
 const Map = dynamic(() => import('@/app/ui/map'), { ssr: false });
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between p-24">
-      <Suspense fallback={Skeleton}>
+      <Suspense fallback={<CardsSkeleton />}>
         <Slider />
       </Suspense>
       {maps.map((map, index) => (
