7a035bfc7d7dd6de2853fd35b9984e43241eb20f
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-08-12 18:31:00 +0200

    fix slider

--- app/ui/minimap.tsx
+++ app/ui/minimap.tsx
@@ -8,14 +8,13 @@ import 'leaflet/dist/leaflet.css';
 
 interface MiniMapProps {
     url: string,
-    cardWidth: number,
 }
 
-export default function MiniMap({ url, cardWidth }: MiniMapProps) {
+export default function MiniMap({ url }: MiniMapProps) {
     return <>
         <div className="relative hover:scale-125 hover:z-30 transition-transform duration-500 hover:cursor-pointer">
             <div className="absolute w-full h-full z-10 bg-black opacity-50 hover:opacity-0 transition-opacity duration-200"></div>
-            <MapContainer center={[48.8566, 2.3522]} zoom={5} style={{ width: `${cardWidth}px` }} className=" z-0 h-[150px] flex-grow-0 flex-shrink-0 basis-1/5 minimap-leaflet" >
+            <MapContainer center={[48.8566, 2.3522]} zoom={5} className=" z-0 h-[150px]  minimap-leaflet" >
                 <TileLayer
                     url={url}
                 // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -26,4 +25,3 @@ export default function MiniMap({ url, cardWidth }: MiniMapProps) {
 };
 
 
-AZEAZE
