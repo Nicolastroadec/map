d79b95646006e27b088d73aadae07a711c873dbd
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-08-12 16:56:18 +0200

    update slider

--- app/ui/minimap.tsx
+++ app/ui/minimap.tsx
@@ -12,12 +12,9 @@ interface MiniMapProps {
 }
 
 export default function MiniMap({ url, cardWidth }: MiniMapProps) {
-
-
-
     return <>
-        <div className="relative">
-            <div className="absolute w-full h-full z-10"></div>
+        <div className="relative hover:scale-125 hover:z-30 transition-transform duration-500 hover:cursor-pointer">
+            <div className="absolute w-full h-full z-10 bg-black opacity-50 hover:opacity-0 transition-opacity duration-200"></div>
             <MapContainer center={[48.8566, 2.3522]} zoom={5} style={{ width: `${cardWidth}px` }} className=" z-0 h-[150px] flex-grow-0 flex-shrink-0 basis-1/5 minimap-leaflet" >
                 <TileLayer
                     url={url}
