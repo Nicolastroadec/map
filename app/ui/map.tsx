1f1d21120aecbd34225fb96386b1167b101f3831
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-08-12 12:10:10 +0200

    fix arrow left and right slider

--- app/ui/map.tsx
+++ app/ui/map.tsx
@@ -4,20 +4,24 @@
 // components/Map.js
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useMemo } from 'react';
+
 
 interface MapProps {
     url: string
 }
 
 export default function Map({ url }: MapProps) {
-    return (
+    // Mémoriser le MapContainer pour éviter la réinitialisation
+    const map = useMemo(() => (
         <MapContainer center={[48.8566, 2.3522]} zoom={5} className="w-2/3 h-[500px] mb-10">
             <TileLayer
                 url={url}
-            // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
         </MapContainer>
-    );
+    ), [url]); // Dépendre uniquement de l'URL
+
+    return map;
 };
 
 
--- app/ui/slider.tsx
+++ app/ui/slider.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import { maps } from '@/app/lib/data'
 import { CardSkeleton } from '@/app/ui/skeletons';
@@ -9,17 +9,49 @@ const MiniMap = dynamic(() => import('@/app/ui/minimap'), { loading: () => <Card
 
 export default function Slider() {
 
+    const [widthCard, setWidthCard] = useState(0);
+    function handleNextClick() {
+        setWidthCard(prevWidth => prevWidth + 420);
+    }
+
+    function handlePreviousClick() {
+        setWidthCard(prewidth => {
+            if (prewidth === 0) {
+                return prewidth;
+            }
+            return prewidth - 420;
+        })
+    }
+
     return (
         <>
-            <div className="w-full h-[200px] overflow-hidden mb-10">
-                <div className="flex">
-                    {maps.map((map, index) => (
-                        <MiniMap key={index} url={map.url} />
-                    ))}
+            <div className="flex relative items-center">
+                <div onClick={handlePreviousClick}>
+                    <svg fill="#fff" className="cursor-pointer h-[40px] w-[40px]" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330" transform="rotate(180)">
+                        <g id="SVGRepo_iconCarrier"> <path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001 c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213 C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606 C255,161.018,253.42,157.202,250.606,154.389z" /> </g>
+                    </svg>
                 </div>
-            </div>
+                <div className="w-full h-[200px] overflow-hidden mb-10">
+                    <div className="flex cards transition-transform duration-300 ease-in-out" style={{ transform: `translateX(-${widthCard}px)` }}
+                    >
+                        {maps.map((map, index) => (
+                            <div key={index} className="card mr-[20px]">
+                                <MiniMap url={map.url} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div onClick={handleNextClick}>
 
+                    <svg fill="#fff" className="cursor-pointer h-[40px] w-[40px]" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 330 330">
+                        <path id="XMLID_222_" d="M250.606,154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213,0.001
+	c-5.857,5.858-5.857,15.355,0.001,21.213l139.393,139.39L79.393,304.394c-5.857,5.858-5.857,15.355,0.001,21.213
+	C82.322,328.536,86.161,330,90,330s7.678-1.464,10.607-4.394l149.999-150.004c2.814-2.813,4.394-6.628,4.394-10.606
+	C255,161.018,253.42,157.202,250.606,154.389z"/>
+                    </svg></div>
+
+            </div >
         </>
     )
-
 }
