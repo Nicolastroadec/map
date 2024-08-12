6290f8b7f39c1c0c59ea473335a3198c03e89ab4
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-08-12 15:51:31 +0200

    update fill available

--- app/ui/slider.tsx
+++ app/ui/slider.tsx
@@ -27,7 +27,7 @@ export default function Slider() {
 
     return (
         <>
-            <div className="container-slider z-20 block w-full max-w-full fixed items-center mb-10">
+            <div className="left-0 right-0 z-20 block fixed items-center mb-10">
                 <div className="relative bg-black flex">
                     <div onClick={handlePreviousClick}>
                         <ArrowLeft />
