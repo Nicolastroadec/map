4ea5320752f36307a4841ba976f930b7c94cd001
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-08-12 18:31:00 +0200

    fix slider

--- app/ui/slider.tsx
+++ app/ui/slider.tsx
@@ -1,28 +1,100 @@
 'use client';
-import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import { maps } from '@/app/lib/data'
 import { CardSkeleton } from '@/app/ui/skeletons';
 import { ArrowLeft, ArrowRight } from '@/app/ui/arrows';
+import React, { useEffect, useRef, useState } from 'react';
+
 
 
 const MiniMap = dynamic(() => import('@/app/ui/minimap'), { loading: () => <CardSkeleton />, ssr: false });
 
 export default function Slider() {
+    const card = useRef<HTMLDivElement>(null);
 
     const [widthCards, setWidthCards] = useState(0);
-    const cardWidth: number = 250;
+
+    const [visibleCards, setVisibleCards] = useState(0)
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const cardsNumber = maps.length;
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidthCards(0);
+            setCurrentIndex(0);
+
+            const windowWidth = window.innerWidth;
+
+            const breakPoints = {
+                verySmall: 480,
+                small: 640,
+                middle: 768,
+                big: 1024,
+                xl: 1280,
+                xxl: 1536
+            };
+
+            if (windowWidth < breakPoints.verySmall) {
+                setVisibleCards(1);
+            } else if (windowWidth < breakPoints.small) {
+                setVisibleCards(2);
+            } else if (windowWidth < breakPoints.middle) {
+                setVisibleCards(3);
+            } else if (windowWidth < breakPoints.big) {
+                setVisibleCards(4);
+            } else if (windowWidth < breakPoints.xl) {
+                setVisibleCards(5);
+            } else if (windowWidth < breakPoints.xxl) {
+                setVisibleCards(6);
+            } else {
+                setVisibleCards(7);
+            }
+
+            setCurrentIndex(visibleCards);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Initial call to handle the current window size
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [visibleCards]);
+
     function handleNextClick() {
-        setWidthCards(prevWidth => prevWidth - (cardWidth + 20));
+        console.log(currentIndex);
+        console.log(cardsNumber);
+        if (currentIndex === cardsNumber) {
+            return;
+        }
+        if (card.current) {
+            const cardWidth = card.current.offsetWidth ? card.current.offsetWidth : 0;
+            setWidthCards(prevWidth => prevWidth - (cardWidth));
+            setCurrentIndex(prevIndex => prevIndex + 1)
+        }
     }
 
     function handlePreviousClick() {
-        setWidthCards(prewidth => {
-            if (prewidth === 0) {
-                return prewidth;
-            }
-            return prewidth + (cardWidth + 20);
-        })
+        if (currentIndex === visibleCards) {
+            return;
+        }
+
+        if (card.current) {
+            setCurrentIndex(prevIndex => prevIndex - 1)
+            console.log(cardsNumber); console.log(currentIndex);
+
+            const cardWidth = card.current.offsetWidth;
+            setWidthCards(prewidth => {
+                if (prewidth === 0) {
+                    return prewidth;
+                }
+                return prewidth + (cardWidth);
+            })
+        }
     }
 
     return (
@@ -33,11 +105,11 @@ export default function Slider() {
                         <ArrowLeft />
                     </div>
                     <div className="w-[90%] h-[200px] overflow-hidden flex items-center">
-                        <div className="cards flex transition-transform duration-300 items-center ease-in-out" style={{ transform: `translateX(${widthCards}px)` }}
+                        <div className="cards w-[100%] flex transition-transform duration-300 items-center ease-in-out" style={{ transform: `translateX(${widthCards}px)` }}
                         >
                             {maps.map((map, index) => (
-                                <div key={index} className="card mr-[20px]">
-                                    <MiniMap cardWidth={cardWidth} url={map.url} />
+                                <div ref={card} key={index} className="card flex-grow-0 flex-shrink-0 2xl:w-[14.28%] xl:w-1/6 lg:w-1/5 md:w-1/4 sm:w-1/3 w-1/2 max-[480px]:w-full pl-[20px]">
+                                    <MiniMap url={map.url} />
                                 </div>
                             ))}
                         </div>
