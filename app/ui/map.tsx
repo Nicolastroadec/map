2471e0f1666cd440819d4635f150468f15325d67
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-08-12 15:37:14 +0200

    fix slider width

--- app/ui/map.tsx
+++ app/ui/map.tsx
@@ -4,7 +4,6 @@
 // components/Map.js
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { useMemo } from 'react';
 
 
 interface MapProps {
@@ -13,15 +12,13 @@ interface MapProps {
 
 export default function Map({ url }: MapProps) {
     // Mémoriser le MapContainer pour éviter la réinitialisation
-    const map = useMemo(() => (
-        <MapContainer center={[48.8566, 2.3522]} zoom={5} className="w-2/3 h-[500px] mb-10">
-            <TileLayer
-                url={url}
-            />
-        </MapContainer>
-    ), [url]); // Dépendre uniquement de l'URL
 
-    return map;
+
+    return <MapContainer center={[48.8566, 2.3522]} zoom={5} className="w-2/3 h-[500px] mb-10 z-0">
+        <TileLayer
+            url={url}
+        />
+    </MapContainer>;
 };
 
 
