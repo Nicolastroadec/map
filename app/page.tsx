f75d6aaa09ae7ebfdcda27c850c8b28f1a89e8e8
Nicolastroadec <troadecnicolas@hotmail.fr>
2024-08-19 14:17:37 +0200

    update text

--- app/page.tsx
+++ app/page.tsx
@@ -43,9 +43,11 @@ export default function Home() {
 
   return (
     <main className=" bg-[#0d1321] min-h-screen md:p-24 pt-20 pl-2 pr-2 mt-40">
-      <h1 className="text-3xl mb-5 ">Une liste de cartes utilisables gratuitement, ou avec certaines limites</h1>
-      <p className="mb-4">Tous les fonds de carte listés ci-dessous peuvent être utilisés gratuitement. Certains peuvent nécessiter de créer un compte, afin de récupérer une clé API ou bien l{`'`}ID d{`'`}un compte.
-        Ces cartes peuvent être utilisées pour y ajouter des markers, des popups, des frontières... Attention à bien lire les conditions d{`'`}utilisation : certaines d{`'`}entre elles ne peuvent pas servir à un usage commercial, et la plupart doivent être utilisées de façon raisonnable.</p>
+      <h1 className="text-3xl mb-5 ">Une liste de cartes utilisables gratuitement</h1>
+      <p className="mb-4">Tous les fonds de carte listés ci-dessous peuvent être utilisés gratuitement.
+        Certains peuvent nécessiter de créer un compte, afin de récupérer une clé API ou bien l{`'`}ID d{`'`}un compte. Pour ces cas-là, l{`'`}usage est limité à un nombre de requêtes mensuelles.
+        Ces cartes peuvent être utilisées pour y ajouter des markers, des popups, des frontières...
+        Attention à bien lire les conditions d{`'`}utilisation : certaines, même gratuites, ne peuvent pas servir à un usage commercial, et la plupart doivent être utilisées de façon raisonnable.</p>
       <h2 className="text-xl mb-4">Application propulsée par Next.js, Tailwind et TypeScript 🚀</h2>
       <Slider indexClicked={(index) => indexClicked(index)} />
       {maps.map((map, index) => (
