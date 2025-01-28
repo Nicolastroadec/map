1f03e5e7b1ce9c77080ff8bf9bf2dc2c9d710db0
Nicolastroadec <troadecnicolas@hotmail.fr>
2025-01-28 15:06:13 +0100

    update

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -13,7 +13,9 @@ interface MapsDetailsData {
 export default function Map({ data }: MapsDetailsData) {
     const { site, name, license, url } = data;
     const attribution = `&copy; <a href=${site}>${name}</a> ${license}`;
-    return <MapContainer center={[48.8566, 2.3522]} zoom={5} className="xl:w-3/5 w-full h-[500px] mb-5 z-0">
+    const id = name?.replace(/ /g, "").toLowerCase();
+
+    return <MapContainer id={id} center={[48.8566, 2.3522]} zoom={5} className="xl:w-3/5 w-full h-[500px] mb-5 z-0">
         <TileLayer
             url={url ? url : 'No url provided'}
             attribution={attribution} />
